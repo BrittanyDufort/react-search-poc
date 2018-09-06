d072ade7801ca3f026ccad11ca507b090fea0745
Brittany Dufort <brittany.dufort@gmail.com>
2018-09-06 14:05:54 -0700

    loading test json and filtering results

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import { Col, Grid, Row } from 'react-bootstrap';
 
 export class Layout extends Component {
-  displayName = Layout.name
+    displayName = Layout.name
 
-  render() {
-    return (
-      <Grid fluid>
-        <Row>
-          <Col sm={3}>
-          </Col>
-          <Col sm={9}>
-            {this.props.children}
-          </Col>
-        </Row>
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <Grid fluid>
+                <Row>
+                    <Col sm={2}>
+                    </Col>
+                    <Col sm={8}>
+                        {this.props.children}
+                    </Col>
+                    <Col sm={2}>
+                    </Col>
+                </Row>
+            </Grid>
+        );
+    }
 }
--- ClientApp/src/components/Search.js
+++ ClientApp/src/components/Search.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+import axios from 'axios';
 
 export class SearchPage extends React.Component {
     render() {
         return (
             <div className="searchPage">
                 <h1>Search</h1>
+                <p><strong>Please search by Album ID (1 - 100)</strong></p>
                 <SearchBox />
             </div>
         );
@@ -26,7 +29,7 @@ class SearchBox extends React.Component {
         this.setState({ inputValue: event.target.value })
     }
 
-    handleClick= () => {
+    handleClick = (event) => {
         this.setState({
             clicked: true
         });
@@ -45,12 +48,30 @@ class SearchBox extends React.Component {
 
 class SearchResults extends React.Component {
 
+    state = {
+        photos: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/photos`)
+            .then(res => {
+                const photos = res.data;
+                this.setState({ photos });
+            })
+    }
+
     render() {
         return (
             <div className="searchResults">
-                <h2>Results</h2>
-                <p>You searched for {this.props.searchValue}.</p>
+                {this.state.photos.filter(
+                    ({ albumId }) => albumId == this.props.searchValue).map(photo =>
+                    <div className="searchResult">
+                        <h4>{photo.title}</h4>
+                        <p>Album ID: {photo.albumId}</p>
+                        <img src={photo.thumbnailUrl} />
+                    </div>
+                )}
             </div>
-        );
+        )
     }
 };
