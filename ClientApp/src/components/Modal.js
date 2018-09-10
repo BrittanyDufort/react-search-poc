e390ed9d1384177f36f0ba9ed39b1ff843da03db
Brittany Dufort <brittany.dufort@gmail.com>
2018-09-10 14:17:59 -0700

    Modal functionality

---
+++ ClientApp/src/components/Modal.js
@@ -0,0 +1,72 @@
+﻿import React, { Component } from 'react';
+import isNil from 'lodash/fp/isNil';
+
+export class Modal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
+
+    // Add listeners immediately after the component is mounted.
+    componentDidMount() {
+        window.addEventListener('keyup', this.handleKeyUp, false);
+        document.addEventListener('click', this.handleOutsideClick, false);
+    }
+
+    // Remove listeners immediately before a component is unmounted and destroyed.
+    componentWillUnmount() {
+        window.removeEventListener('keyup', this.handleKeyUp, false);
+        document.removeEventListener('click', this.handleOutsideClick, false);
+    }
+
+    // Handle the key press event of ESC to close modal.
+    handleKeyUp(e) {
+        const { onCloseRequest } = this.props;
+        const keys = {
+            27: () => {
+                e.preventDefault();
+                onCloseRequest();
+                window.removeEventListener('keyup', this.handleKeyUp, false);
+            },
+        };
+
+        if (keys[e.keyCode]) { keys[e.keyCode](); }
+    }
+
+    // Handle the mouse click on browser window to close modal.
+    handleOutsideClick(e) {
+        const { onCloseRequest } = this.props;
+
+        if (!isNil(this.modal)) {
+            if (!this.modal.contains(e.target)) {
+                onCloseRequest();
+                document.removeEventListener('click', this.handleOutsideClick, false);
+            }
+        }
+    }
+
+    render() {
+        const {
+            onCloseRequest,
+            children,
+        } = this.props;
+
+        return (
+            <div className="modalOverlay">
+                <div className="modal-content">
+                    <button type="button" className="closeButton" onClick={onCloseRequest}> &times; </button>
+                    <div className="modal-body" ref={node => (this.modal = node)}>
+                        <div className="modalContent">
+                            {children}
+                            <p>Some text in the Modal Body</p>
+                            <p>Some other text...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
--- ClientApp/src/components/Search.js
+++ ClientApp/src/components/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Grid, Row } from 'react-bootstrap';
 import axios from 'axios';
+import { Modal } from './Modal';
 
 export class SearchPage extends React.Component {
     render() {
@@ -48,10 +49,14 @@ class SearchBox extends React.Component {
 
 class SearchResults extends React.Component {
 
-    state = {
-        photos: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            photos: []
+        };
     }
 
+    //Grabs data (test json) on component mount
     componentDidMount() {
         axios.get(`https://jsonplaceholder.typicode.com/photos`)
             .then(res => {
@@ -63,14 +68,46 @@ class SearchResults extends React.Component {
     render() {
         return (
             <div className="searchResults">
+
                 {this.state.photos.filter(
                     ({ albumId }) => albumId == this.props.searchValue).map(photo =>
-                    <div className="searchResult">
-                        <h4>{photo.title}</h4>
-                        <p>Album ID: {photo.albumId}</p>
-                        <img src={photo.thumbnailUrl} />
-                    </div>
-                )}
+                        <SearchResult photo={photo} />
+                    )}
+            </div>
+        )
+    }
+};
+
+class SearchResult extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false
+        };
+    }
+
+    handleToggleModal() {
+        this.setState({ showModal: !this.state.showModal });
+    }
+
+    render() {
+        const { showModal } = this.state;
+
+        return (
+            <div className="searchResult" id={this.props.photo.id}>
+                <h4>{this.props.photo.title}</h4>
+                <p>Album ID: {this.props.photo.albumId}</p>
+                <a src="#"
+                    className="modalButton"
+                    onClick={() => this.handleToggleModal()}>
+                    <img src={this.props.photo.thumbnailUrl} />
+                </a>
+
+                {showModal &&
+                    <Modal onCloseRequest={() => this.handleToggleModal()}>
+                        <img src={this.props.photo.url} alt={this.props.photo.title} />
+                    </Modal>
+                }
             </div>
         )
     }
